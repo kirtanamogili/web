5b4bd2abdbab56682d0b400e8bf21997a9c5c29a
Kirtana Mogili <kirtana2000@gmail.com>
2021-07-26 11:54:07 -0400

    Error message

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,17 +7,8 @@ const Resume = () => {
             <hr></hr>
             <h4>More of my credentials.</h4>
             <div>
-                    <body className="text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis felis eu orci bibendum venenatis blandit in mi. Etiam mollis viverra ultrices. Praesent urna ex, volutpat ac vestibulum eget, consectetur eu lorem. Praesent et hendrerit quam. Phasellus in suscipit ante, vel vehicula ipsum. Proin sed tortor nec nisi vehicula elementum sit amet vitae felis. Proin sagittis hendrerit nunc a semper. In venenatis augue auctor metus tincidunt, vitae sagittis turpis bibendum. Vivamus sodales arcu arcu, a pretium lacus rhoncus et. Proin neque sapien, tincidunt at imperdiet sed, finibus ac dui. Nam in dignissim lectus. Curabitur placerat aliquam efficitur. Aliquam sapien dui, convallis vitae turpis ac, tincidunt consequat velit. In vel maximus libero, vitae hendrerit odio. Nulla facilisi. Mauris venenatis, ligula non consequat vehicula, sapien diam rhoncus enim, ac lacinia enim magna quis enim.
-                    </body>
-                    <body className="text">
-                    Nam pharetra scelerisque dapibus. Nam odio turpis, blandit et commodo non, accumsan vitae sapien. Nullam fringilla pharetra est sed auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean leo leo, accumsan ut sem et, consequat dapibus mauris. Sed eu nisi vitae orci posuere consectetur ut vitae dolor. Curabitur ultricies rhoncus massa semper eleifend. Nullam in malesuada diam. Morbi mollis rutrum vestibulum. Etiam dictum nec leo ut gravida. Suspendisse a malesuada eros, nec lobortis libero.
-                    </body>
-                    <body className="text">
-                    Ut luctus et felis nec pretium. Sed nibh massa, euismod vitae posuere eget, vehicula sit amet lorem. Sed porttitor magna nisi, sit amet mattis nunc ultrices nec. Suspendisse eleifend pellentesque quam, quis porttitor elit auctor in. Sed eu ligula feugiat, hendrerit justo a, mattis quam. Mauris rhoncus tortor purus, at dapibus ligula fermentum sit amet. Suspendisse ullamcorper convallis nunc eu fermentum. Donec a nulla a elit sollicitudin finibus et sit amet sem. Etiam vitae neque non mauris auctor posuere. Sed suscipit vitae turpis at venenatis. Etiam iaculis turpis et lacinia dignissim.
-                    </body>
-                    <img></img>
-                </div>
+                <iframe title="resume" href="../../public/Resume.pdf" width="100%" height="100%"></iframe>
+            </div>
         </div>
     )
 }
