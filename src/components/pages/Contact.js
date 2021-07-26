1f9353bb6fafa391cbddb16f6f9bb1d0dda05d8a
Kirtana Mogili <kirtana2000@gmail.com>
2021-07-26 11:25:42 -0400

    Additional modifications

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
                 <h4>Let's connect.</h4>
                 <div className="contact-body">
                     <body className="text">
-                        I am currently looking for 2021/2022 internship opportunities. Please shoot my an email or connect with me on LinkedIn to start a conversation. I would love to get to know you better and share more about myself!<br></br>
+                        I am currently looking for 2021/2022 internship opportunities. Please shoot me an email or connect with me on LinkedIn to start a conversation. I would love to get to know you better and share more about myself!<br></br>
                         <br></br>
                     </body>
                     <a href="mailto: kirtanamogili@gatech.edu" class="contact">Say Hello!</a>
